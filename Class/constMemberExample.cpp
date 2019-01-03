a327af57c671db9a31e29d44841749db6a1bd6a8
Mohitmohan Ingale <mohitmohan.i@HCLCA.COM>
2019-01-03 12:24:11 +0530

    Pass by reference

--- Class/constMemberExample.cpp
+++ Class/constMemberExample.cpp
@@ -9,8 +9,8 @@ class constExample
     public:
         constExample(int d1, int d2): data1(d1),data2(d2)
         {}
-        /* constExample()
-        {} */
+        constExample()
+        {}
         int addFunction() const;
 };
 
@@ -21,10 +21,25 @@ int constExample::addFunction() const
     return sum;
 }
 
+int swap(int & data1, int & data2)
+{
+    int temp = data1;
+    data1 = data2;
+    data2 = data1;
+}
+
 int main()
 {
     constExample c1(10,5),c2(6,8),c3;
+    int val1 =7, val2 = 9;
     cout << c1.addFunction() << endl;
     cout << c2.addFunction() << endl;
+    cout <<"Before Swap" << endl;
+    cout << val1 << endl;
+    cout << val2 << endl;
+    swap(val1,val2);
+    cout <<"After Swap" << endl;
+    cout << val1 << endl;
+    cout << val2 << endl;
     return 0;
 }
