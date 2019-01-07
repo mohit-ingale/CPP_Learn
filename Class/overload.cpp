ff080985b4c0e802dc83fb104dbcf70d7b601c4e
Mohitmohan Ingale <mohitmohan.i@HCLCA.COM>
2019-01-07 18:30:34 +0530

    Overload test

---
+++ Class/overload.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <string.h>
+
+using namespace std;
+
+class HomeBrew
+{
+    private:
+        int len;
+        char str[50];
+        static int i;
+    public:
+        HomeBrew()
+        {}
+        HomeBrew(char * iStr)
+        {
+            int i = 0;
+            while(*iStr)
+                str[i++] = *iStr;
+            str[i] = '\0';
+        }
+        void operator ++ ()
+        {
+
+            str[i] = 'c';
+        }
+};
+int HomeBrew:: i = 0;
+int main()
+{
+    HomeBrew h1 = "Helllo";
+    h1++;
+    return 0;
+}
