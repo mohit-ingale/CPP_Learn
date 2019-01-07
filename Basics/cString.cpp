8e2ef2ec5fbba3f23c640da008ecf23668ca0559
Mohitmohan Ingale <mohitmohan.i@HCLCA.COM>
2019-01-07 17:32:41 +0530

    Test

---
+++ Basics/cString.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+int main()
+{
+    char name[10];
+    std::cout << "Enter you name" << std::endl;
+    std::cin >> name;
+    std::cout << "Name is " << name << std::endl;
+}
