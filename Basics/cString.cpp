49a3d14e32513bcb0639d15eecb35e8d7fd06655
Mohitmohan Ingale <mohitmohan.i@HCLCA.COM>
2019-01-07 17:37:47 +0530

    Stream to get data

--- Basics/cString.cpp
+++ Basics/cString.cpp
@@ -4,6 +4,7 @@ int main()
 {
     char name[10];
     std::cout << "Enter you name" << std::endl;
-    std::cin >> name;
+    //std::cin >> name; //If we enter space it will terminate
+    std::cin.get(name,10);
     std::cout << "Name is " << name << std::endl;
 }
